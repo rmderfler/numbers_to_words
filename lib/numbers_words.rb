a1f4f2cbc16ba10d14e185dba89524ac4d0368b9
Anna Valkov & Michael Derfler <student@epicodus.com>
2014-07-24 11:04:40 -0700

    can take 2 digit numbers and retunr them as a word

--- lib/numbers_words.rb
+++ lib/numbers_words.rb
@@ -1,11 +1,21 @@
 def num_words (number)
-  number_hash = {1 => "one", 2 => "two", 3 => "three", 4 => "four", 5 => "five",
-                6 => "six", 7 => "seven", 8 => "eight", 9 => "nine", 10 => "ten",
-                11 => "eleven", 12 => "twelve", 13 => "thirteen", 14 => "fourteen",
-                15 => "fifteen", 16 => "sixteen", 17 => "seventeen", 18 => "eighteen",
-                19 => "nineteen"}
-  array = []
+  number = number.to_i
+  number_hash = {90 => "ninety", 80 => "eighty", 70 => "seventy",60 => "sixty",
+                 50 => "fifty",40 => "forty",30 => "thirty",20 => "twenty",19 => "nineteen",
+                 18 => "eighteen",17 => "seventeen",16 => "sixteen",15 => "fifteen", 14 => "fourteen",
+                 13 => "thirteen", 12 => "twelve",11 => "eleven",10 => "ten",9 => "nine",8 => "eight",
+                 7 => "seven", 6 => "six",5 => "five",4 => "four",3 => "three",2 => "two", 1 => "one",}
 
-  result = number_hash[number]
-  result
+  array = []
+    0.upto(number) do
+      number_hash.each do |num, word|
+        if number >= num
+          array << word
+          number -= num
+          break
+        end
+      end
+    end
+  words = array.join(" ")
+  words
 end
--- spec/number_words_spec.rb
+++ spec/number_words_spec.rb
@@ -3,12 +3,16 @@ require('numbers_words.rb')
 
 describe('num_words') do
 
-  it('take a single digit number and returns it in words')do
+  it('takes a single digit number and returns it in words')do
   num_words(6).should(eq("six"))
   end
 
-  it('take a teen-number and returns it in words')do
+  it('takes a teen-number and returns it in words')do
   num_words(16).should(eq("sixteen"))
+  end
+
+  it('takes a double digit number and returns it in words')do
+  num_words(24).should(eq("twenty four"))
   end
 
 end
