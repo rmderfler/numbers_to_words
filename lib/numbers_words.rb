cfe9da238ea3078cf2291d6962123ea5939ee9f5
Anna Valkov & Michael Derfler <student@epicodus.com>
2014-07-24 13:35:59 -0700

    6001 working

--- lib/numbers_words.rb
+++ lib/numbers_words.rb
@@ -1,10 +1,18 @@
 def num_words (number)
+
   number = number.to_i
-  number_hash = {90 => "ninety", 80 => "eighty", 70 => "seventy",60 => "sixty",
-                 50 => "fifty",40 => "forty",30 => "thirty",20 => "twenty",19 => "nineteen",
-                 18 => "eighteen",17 => "seventeen",16 => "sixteen",15 => "fifteen", 14 => "fourteen",
-                 13 => "thirteen", 12 => "twelve",11 => "eleven",10 => "ten",9 => "nine",8 => "eight",
-                 7 => "seven", 6 => "six",5 => "five",4 => "four",3 => "three",2 => "two", 1 => "one",}
+  number_hash = {900 => "nine hundred",800 => "eight hundred",700 => "seven hundred",
+                600 => "six hundred",500 => "five hundred",400 => "four hundred",
+                300 => "three hundred",200 => "two hundred",100 => "hundred",
+                90 => "ninety", 80 => "eighty", 70 => "seventy",60 => "sixty",
+                50 => "fifty",40 => "forty",30 => "thirty",20 => "twenty",
+                19 => "nineteen",18 => "eighteen",17 => "seventeen",16 => "sixteen",
+                15 => "fifteen", 14 => "fourteen",13 => "thirteen", 12 => "twelve",
+                11 => "eleven",10 => "ten",9 => "nine",8 => "eight",7 => "seven",
+                6 => "six",5 => "five",4 => "four",3 => "three",2 => "two", 1 => "one"}
+
+  powers_hash = {10**3 => "thousand", 10**6 => "million",
+               10**9 => "billion", 10**12 => "trillion"}
 
   array = []
     0.upto(number) do
@@ -19,3 +27,10 @@ def num_words (number)
   words = array.join(" ")
   words
 end
+
+def high_numbers (number)
+  number = number.to_i
+  new_num = number/1000
+  result = num_words(new_num)
+  result = result + " thousand"
+  end
--- spec/number_words_spec.rb
+++ spec/number_words_spec.rb
@@ -4,15 +4,27 @@ require('numbers_words.rb')
 describe('num_words') do
 
   it('takes a single digit number and returns it in words')do
-  num_words(6).should(eq("six"))
+    num_words(6).should(eq("six"))
   end
 
   it('takes a teen-number and returns it in words')do
-  num_words(16).should(eq("sixteen"))
+    num_words(16).should(eq("sixteen"))
   end
 
   it('takes a double digit number and returns it in words')do
-  num_words(24).should(eq("twenty four"))
+    num_words(24).should(eq("twenty four"))
+  end
+
+  it('takes a three digit number and returns it in words')do
+    num_words(248).should(eq("two hundred forty eight"))
+  end
+
+end
+
+describe('high_numbers') do
+
+  it('takes a single digit number and returns it in words')do
+    high_numbers(6000).should(eq("six thousand"))
   end
 
 end
